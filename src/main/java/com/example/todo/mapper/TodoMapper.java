84740710190f2cbb6ee0fc5d12ec2e5aba876d2e
Six Eggs <sixeggs0303@gmail.com>
2020-12-10 19:07:49 +0800

    [TodoMapper][Marcus Luk] Feat: get labels object for todo response

--- src/main/java/com/example/todo/mapper/TodoMapper.java
+++ src/main/java/com/example/todo/mapper/TodoMapper.java
@@ -2,12 +2,21 @@ package com.example.todo.mapper;
 
 import com.example.todo.dto.TodoRequest;
 import com.example.todo.dto.TodoResponse;
+import com.example.todo.model.Label;
 import com.example.todo.model.Todo;
+import com.example.todo.service.LabelService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TodoMapper {
+    @Autowired
+    private LabelService labelService;
+
     public Todo toEntity(TodoRequest todoRequest) {
         Todo todo = new Todo();
 
@@ -16,10 +25,18 @@ public class TodoMapper {
         return todo;
     }
 
-    public TodoResponse toResponse(Todo todo) {
+    public TodoResponse toResponse(Todo todo) throws Exception {
         TodoResponse todoResponse = new TodoResponse();
 
         BeanUtils.copyProperties(todo, todoResponse);
+
+        List<Label> labels = new ArrayList<>();
+
+        for (String labelId : todo.getLabelIds()) {
+            labels.add(labelService.getLabel(labelId));
+        }
+
+        todoResponse.setLabels(labels);
 
         return todoResponse;
     }

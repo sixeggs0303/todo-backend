cd012551b7b6e73b5c43e11eac0fa6512233a355
Six Eggs <sixeggs0303@gmail.com>
2020-12-10 17:36:38 +0800

    [TodoService][Marcus Luk] Fix: changed using request and http status code for controller

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -1,12 +1,14 @@
 package com.example.todo.controller;
 
 
+import com.example.todo.dto.TodoRequest;
 import com.example.todo.dto.TodoResponse;
 import com.example.todo.exception.TodoNotFoundException;
 import com.example.todo.mapper.TodoMapper;
 import com.example.todo.model.Todo;
 import com.example.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,16 +34,20 @@ public class TodoController {
     }
 
     @PostMapping()
-    public TodoResponse createTodo(@RequestBody Todo todo) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public TodoResponse createTodo(@RequestBody TodoRequest todoRequest) {
+        Todo todo = todoMapper.toEntity(todoRequest);
         return todoMapper.toResponse(todoService.createTodo(todo));
     }
 
     @PutMapping("/{id}")
-    public TodoResponse updateTodo(@PathVariable String id, @RequestBody Todo updatedTodo) throws TodoNotFoundException {
+    public TodoResponse updateTodo(@PathVariable String id, @RequestBody TodoRequest updatedTodoRequest) throws TodoNotFoundException {
+        Todo updatedTodo = todoMapper.toEntity(updatedTodoRequest);
         return todoMapper.toResponse(todoService.updateTodo(id, updatedTodo));
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTodo(@PathVariable String id) throws TodoNotFoundException {
         todoService.deleteTodo(id);
     }

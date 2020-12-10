bda96a43f9df875713d1a5124e5b90e92f72747a
Six Eggs <sixeggs0303@gmail.com>
2020-12-10 15:59:58 +0800

    [TodoBackend][Marcus Luk] First Commit

---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,57 @@
+package com.example.todo.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
+
+@Document
+public class Todo {
+    @MongoId
+    private String id;
+    private String text;
+    private boolean done;
+    private List<String> labelIds;
+
+    public Todo() {
+    }
+
+    public Todo(String id, String text, boolean done, List<String> labelIds) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+        this.labelIds = labelIds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public List<String> getLabelIds() {
+        return labelIds;
+    }
+
+    public void setLabelIds(List<String> labelIds) {
+        this.labelIds = labelIds;
+    }
+}
---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.example.todo.repository;
+
+import com.example.todo.model.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
